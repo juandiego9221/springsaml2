05898a59de31be01164acb2a83de985639b8da9e
Juan Diego <juan.mamani.contractor@bbva.com>
2020-12-27 20:44:32 -0500

    configuracion 1

--- src/main/java/pe/com/jdmm21/springsecurity/demo/config/SpringSecurityConfig.java
+++ src/main/java/pe/com/jdmm21/springsecurity/demo/config/SpringSecurityConfig.java
@@ -1,12 +1,24 @@
 package pe.com.jdmm21.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import static org.springframework.security.extensions.saml2.config.SAMLConfigurer.saml;
+//import static org.springframework.security.extensions.saml2.config.SAMLConfigurer.saml;
+
 @EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${security.saml2.metadata-url}")
+    String metadataUrl;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("admin")
@@ -24,7 +36,26 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER")
-                .and().httpBasic();
+//        http.authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER")
+//                .and().httpBasic();
+        http.authorizeRequests().antMatchers("/saml/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .apply(saml())
+                .serviceProvider()
+                .keyStore()
+                .storeFilePath("saml/keystore.jks")
+                .password("secret")
+                .keyname("spring")
+                .keyPassword("secret")
+                .and()
+                .protocol("https")
+                .hostname("localhost:8443")
+                .basePath("/")
+                .and()
+                .identityProvider()
+                .metadataFilePath(metadataUrl)
+                .and();
+
     }
 }
